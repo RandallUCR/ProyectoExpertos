e4bd0c233428209a95547427880aa5a192bcd94d
Randall <randallgears1998@gmail.com>
2021-05-19 20:47:50 -0600

    Correcion imagenes

--- Proyecto1_Expertos_G5/View/recomendacionView.php
+++ Proyecto1_Expertos_G5/View/recomendacionView.php
@@ -1,28 +1,28 @@
 <?php
     include_once 'View/headerView.php';
 ?>
-			  
+
 			<!-- start banner Area -->
 			<section class="about-banner relative">
 				<div class="overlay overlay-bg"></div>
-				<div class="container">				
+				<div class="container">
 					<div class="row d-flex align-items-center justify-content-center">
 						<div class="about-content col-lg-12">
 							<h1 class="text-white">
-								Destinos Turisticos Recomendados				
-							</h1>	
-						</div>	
+								Destinos Turisticos Recomendados
+							</h1>
+						</div>
 					</div>
 				</div>
 			</section>
-			<!-- End banner Area -->	
+			<!-- End banner Area -->
 
 			<!-- Start destinations Area -->
 			<section class="destinations-area section-gap">
 				<div class="container">
-					
+
 					<div class="row">
-						<?php 
+						<?php
 						foreach($vars['DestinosR'] as $item)
 
 						{
@@ -32,40 +32,39 @@
 							<form action="?controlador=Index&accion=detallesDestino" method="post">
 								<div class="single-destinations">
 									<div class="thumb">
-										<img src="<?php echo $item[2]?>" alt="" width="100%" height="35%">
+										<img src="<?php echo $item[2]?>" alt="" width="100%" height="auto" style="max-height:200px">
 									</div>
 									<div class="details">
 										<h4 class="d-flex justify-content-between">
-											<span><?php echo $item[1]?></span>                              	
+											<span><?php echo $item[1]?></span>
 											<input type="hidden" id="idDestino" name="idDestino"  value="<?php echo $item[0]?>"/> <br>
 											<input type="hidden" id="tipoTurismo" name="tipoTurismo"  value="<?php echo $item[3]?>"/> <br>
 										</h4>
 
 										<ul class="package-list">
-														
+
 											<input class="genric-btn primary e-large" type="submit"  value="Ver más"/> <br><br>
-												
+
 										</ul>
 									</div>
 								</div>
 							</form>
 
-									
+
 						</div>
-							
+
 						<?php
 						}
 						?>
 
-			
+
 					</div>
-									
-						
-				</div>	
+
+
+				</div>
 			</section>
 			<!-- End destinations Area -->
-			
+
 <?php
     include_once 'View/footerView.php';
 ?>
-
